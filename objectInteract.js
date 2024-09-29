959541bdd81f8c91a7d62ac57b7d69e568e48f95
양정화 <siltarre@gmail.com>
2024-09-29 09:16:09 +0900

    여러 아이템 추가 타입 추가

--- objectInteract.js
+++ objectInteract.js
@@ -14,17 +14,15 @@ function findKey() {
 function findDoor() {
   const door = { item: "Door", type: "door", description: "The main exit door, it's locked.", image: "images/door.png" };
   showAcquisitionPopup(door.item, door.image);  // Show popup for door
-  // alert("The door is locked. Find the key to open it.");  // Interaction logic for the door
 }
 
 function findLock() {
   const lock = { item: "Lock", type: "lock", description: "A sturdy lock. It requires a specific key to open.", image: "images/lock.png" };
-  addItemToInventory(lock);
   showAcquisitionPopup(lock.item, lock.image);  // Show popup for lock
 }
 
 function findSecondKey() {
-  const secondKey = { item: "Second Key", type: "key", description: "A key that might open the door or the lock.", image: "images/secondKey.png" };
+  const secondKey = { item: "Second Key", type: "lockKey", description: "A key that might open the door or the lock.", image: "images/secondKey.png" };
   addItemToInventory(secondKey);
   showAcquisitionPopup(secondKey.item, secondKey.image);  // Show popup for second key
 }
